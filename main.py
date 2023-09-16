af9b98c538802766f5b1cdf921d9e2e88d74375f
Gahusz <kacperdra@gmail.com>
2023-09-16 14:51:48 +0200

    boardv4

--- main.py
+++ main.py
@@ -1,70 +1,87 @@
-import pprint
 
-import numpy
-board1 = {'top-L': 'X', 'top-M': 'X', 'top-R': 'X', 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+import plansza
+from plansza import print_board
+from plansza import check_win
+#import numpy
+global win
+
+board1 = {'1': ' ', '2': ' ', '3': ' ',
+          '4': ' ', '5': ' ', '6': ' ',
+          '7': ' ', '8': ' ', '9': ' '}
+
 #pprint.pprint(board1)
-
-# def print_board(board):
-#     print('-----')
-#     print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
-#     print('-----')
-#     print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
-#     print('-----')
-#     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
-#     print('-----')
-#
-#
-# print_board(board1)
-# print('Tic Tac Toe game :) Do you want X or O?Type 1 - X, 2 - O and press enter')
-#
-#
-#
-# #while True is infinite loop which can be stopped by for example 'break'
-# while True:
-#     who_starts = input()
-#     if who_starts == '1':
-#         starts = 'X'
-#         break
-#     elif who_starts == '2':
-#         starts = 'O'
-#         break
-#     else:
-#         print('Write \'1\' or \'2\' please!')
-#         print('Do you want X or O? 1 - X, 2 - O and enter')
-#
-#
-#
-# print(who_starts)
-# print('-----')
-# print('1' + '|' + '2' + '|' + '3')
-# print('-----')
-# print('4' + '|' + '5' + '|' + '6')
-# print('-----')
-# print('7' + '|' + '8' + '|' + '9')
-# print('-----')
-# print('choose field please!: 1,2,3,4,5,6,7,8 or 9')
-# field=input()
-pip --version
-#values = board1.values()
-
-#horizontal
-for i in values.arange(0, len(values), 3):
-    if values[i:i+2] == 'X':
-        print('X wins!')
-    if values[i:i+2] == 'O':
-        print('O wins!')
-# if i
-# vertical
-# if
+#print_board(board1)
 
 
-# keys=list(board1.keys())
-# keys[1]
-# for i in keys:
-#     print(i)
+print('Tic Tac Toe game :) Do you want X or O? Type 1 - X, 2 - O and press enter')
 
 
 
+#while True is infinite loop which can be stopped by for example 'break'
+while True:
+    who_starts = input()
+    if who_starts == '1':
+        starts = 'X'
+        break
+    elif who_starts == '2':
+        starts = 'O'
+        break
+    else:
+        print('Write \'1\' or \'2\' please!')
+        print('Do you want X or O? 1 - X, 2 - O and enter')
 
-#print('\n'+ board1['top-L'])
+
+
+print(who_starts)
+print('-----')
+print('1' + '|' + '2' + '|' + '3')
+print('-----')
+print('4' + '|' + '5' + '|' + '6')
+print('-----')
+print('7' + '|' + '8' + '|' + '9')
+print('-----')
+print('choose field please!: 1,2,3,4,5,6,7,8 or 9')
+
+while True:
+    if starts== 'X':
+        field = input()
+        values = list(board1.values())
+        if field in board1:
+            if board1[field]==' ':
+                board1[field]='X'
+                check_win()
+                if win==True:
+                    break
+                else:
+                    starts='O'
+            else:
+                print('This field is taken. Choose other field')
+        else:
+            print('Wrong field.' + 'Choose field from: ' + list(board1.keys()))
+
+    elif starts == 'O':
+        field = input()
+        values = list(board1.values())
+        if field in board1:
+            if board1[field] == ' ':
+                board1[field] = 'O'
+                check_win()
+                if win==True:
+                    break
+                else:
+                    starts='X'
+        else:
+            print('This field is taken. Choose other field')
+    else:
+        print('Wrong field.' + 'Choose field from: ' + list(board1.keys()))
+
+
+#indent - akapit
+
+check_win()
+
+
+
+
 
--- plansza.py
+++ plansza.py
@@ -1,4 +1,65 @@
-board = {'top-L': '', 'top-M': '', 'top-R': '', 'mid-L': '', 'bot-M': '', 'top-R': '', 'low-L': '', 'low-M': '', 'low-R': ''}
-pprint.pprint(board)
+import numpy
+import pprint
+def print_board(board):
+    print('-----')
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print('-----')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-----')
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+    print('-----')
 
-print('hello')
+
+
+def check_win():
+    global win
+    win = False
+    board1 = {'1': ' ', '2': ' ', '3': ' ',
+              '4': ' ', '5': ' ', '6': ' ',
+              '7': ' ', '8': ' ', '9': ' '}
+    values = list(board1.values())
+
+    #horizontal
+
+    # THIS IS WRONG beacuse I can't compare to single character to get good result
+    # for i in numpy.arange(0, len(values), 3):
+    #     if values[i:i+2] == 'X':
+    #         print('X wins!')
+    #     if values[i:i+2] == 'O':
+    #         print('O wins!')
+    #I have to use list comprehension
+    for i in numpy.arange(0, len(values), 3):
+        if all(x == 'X' for x in values[i:i+3]):
+            print('X wins!')
+            win=True
+        if all(o == 'O' for o in values[i:i+3]):
+            print('O wins!')
+            win=True
+
+    #vertical
+
+    for i in numpy.arange(0, 3):
+        if all(x == 'X' for x in values[i::3]): #start:stop:step i::3 is start from i end empty step 3
+            print('X wins!')
+            x=True
+        if all(o == 'O' for o in values[i::3]):
+            print('O wins!')
+            x=True
+
+
+    # diagonally
+    diagonal1=[0, 4, 8]
+    diagonal2=[2, 4, 6]
+
+    if all(values[x] == 'X' for x in diagonal1): #if I dont write =='X' it will check if field is empty only
+        print('X wins!')
+        x = True
+    if all(values[o] == 'O' for o in diagonal1):
+        print('O wins!')
+        x = True
+    if all(values[x] == 'X' for x in diagonal2):
+        print('X wins!')
+        x = True
+    if all(values[o] == 'O' for o in diagonal2):
+        print('O wins!')
+        x = True
