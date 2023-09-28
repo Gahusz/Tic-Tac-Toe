40d067ace298901166c0d6bb23faf5b5aa0cff91
Bartek Lipiński <szanur@gmail.com>
2023-09-28 15:45:29 +0200

    # restart problem

--- functions.py
+++ functions.py
@@ -1,12 +1,19 @@
-import numpy
 import sys
+import numpy
+
+again = ' '
+
+starts='X'
 
 #dictionary
 board1 = {'1': ' ', '2': ' ', '3': ' ',
           '4': ' ', '5': ' ', '6': ' ',
           '7': ' ', '8': ' ', '9': ' '}
+#############################################################
+
 
 def print_board(board):
+
     print('-----')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
     print('-----')
@@ -14,7 +21,9 @@ def print_board(board):
     print('-----')
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
     print('-----')
+#############################################################
 
+#############################################################
 def who_starts():
     global starts
     print('Tic Tac Toe game :) Do you want X or O? Type \'1\' for X, \'2\' for O and press enter')
@@ -25,23 +34,38 @@ def who_starts():
     while True:
         starts = input()
         if starts == '1':
-            starts = 'X'
-            break
+            return 'X'
         elif starts == '2':
-            starts = 'O'
-            break
+            return 'O'
         else:
             print('Write \'1\' or \'2\' please!')
             print('Do you want X or O? 1 - X, 2 - O and enter')
 
 ####################################################################
 
+def restart():
+    global board1
+    global again
+
+    while True:
+        print('Do you want to play again? 1 - yes 2 - no')
+        again = input()
+        if again == "1":
+            break
+        elif again == "2":
+            print('Goodbye')
+            sys.exit(0)
+        else:
+            print('Please enter \'1\' or \'2\'.')
+#############################################################
+
+
 #############################################################
 def choosing_field(starting):
-
+    global board1
     #nested functions
     def check_win(board):
-        global win
+        global board1
         values = list(board.values())
 
         # horizontal
@@ -56,24 +80,32 @@ def choosing_field(starting):
         for i in numpy.arange(0, len(values), 3):
             if all(x == 'X' for x in values[i:i + 3]):
                 print('X wins!')
-                win = True
-                sys.exit()
+                board1 = {'1': ' ', '2': ' ', '3': ' ',
+                          '4': ' ', '5': ' ', '6': ' ',
+                          '7': ' ', '8': ' ', '9': ' '}
+                restart()
             if all(o == 'O' for o in values[i:i + 3]):
                 print('O wins!')
-                win = True
-                sys.exit()
+                board1 = {'1': ' ', '2': ' ', '3': ' ',
+                          '4': ' ', '5': ' ', '6': ' ',
+                          '7': ' ', '8': ' ', '9': ' '}
+                restart()
 
         # vertical
 
         for i in numpy.arange(0, 3):
             if all(x == 'X' for x in values[i::3]):  # start:stop:step i::3 is start from i end empty step 3
                 print('X wins!')
-                win = True
-                sys.exit()
+                board1 = {'1': ' ', '2': ' ', '3': ' ',
+                          '4': ' ', '5': ' ', '6': ' ',
+                          '7': ' ', '8': ' ', '9': ' '}
+                restart()
             if all(o == 'O' for o in values[i::3]):
                 print('O wins!')
-                win = True
-                sys.exit()
+                board1 = {'1': ' ', '2': ' ', '3': ' ',
+                          '4': ' ', '5': ' ', '6': ' ',
+                          '7': ' ', '8': ' ', '9': ' '}
+                restart()
 
         # diagonally
         diagonal1 = [0, 4, 8]
@@ -81,34 +113,38 @@ def choosing_field(starting):
 
         if all(values[x] == 'X' for x in diagonal1):  # if I dont write =='X' it will check if field is empty only
             print('X wins!')
-            win = True
-            sys.exit()
+            board1 = {'1': ' ', '2': ' ', '3': ' ',
+                      '4': ' ', '5': ' ', '6': ' ',
+                      '7': ' ', '8': ' ', '9': ' '}
+            restart()
         if all(values[o] == 'O' for o in diagonal1):
             print('O wins!')
-            win = True
-            sys.exit()
+            board1 = {'1': ' ', '2': ' ', '3': ' ',
+                      '4': ' ', '5': ' ', '6': ' ',
+                      '7': ' ', '8': ' ', '9': ' '}
+            restart()
         if all(values[x] == 'X' for x in diagonal2):
             print('X wins!')
-            win = True
-            sys.exit()
+            board1 = {'1': ' ', '2': ' ', '3': ' ',
+                      '4': ' ', '5': ' ', '6': ' ',
+                      '7': ' ', '8': ' ', '9': ' '}
+            restart()
         if all(values[o] == 'O' for o in diagonal2):
             print('O wins!')
-            win = True
-            sys.exit()
+            board1 = {'1': ' ', '2': ' ', '3': ' ',
+                      '4': ' ', '5': ' ', '6': ' ',
+                      '7': ' ', '8': ' ', '9': ' '}
+            restart()
 
-            # if win:
-            #     while True:
-            #         print('Do you want to play again? 1 - yes 2 - no')
-            #         again = input()
-            #         if again == '1':
-            #             y=' '
-            #             board1 = dict.fromkeys(board1,  y)
-            #             win = False
-            #             break
-            # elif again == '2':
-            #     sys.exit()
-            # else:
-            #     print('Please write \'1\' or \'2\'')
+            #draw
+        if ' ' not in values:
+            print('Draw!')
+            board1 = {'1': ' ', '2': ' ', '3': ' ',
+                      '4': ' ', '5': ' ', '6': ' ',
+                      '7': ' ', '8': ' ', '9': ' '}
+            restart()
+
+
 #########################################################
     while True:
         if starting == 'X':
@@ -119,6 +155,8 @@ def choosing_field(starting):
                     print_board(board1)
                     check_win(board1)
                     starting = 'O'
+                    if again == "1":
+                        break
                 else:
                     print('This field is taken. Choose other field')
 
@@ -134,8 +172,14 @@ def choosing_field(starting):
                     print_board(board1)
                     check_win(board1)
                     starting = 'X'
+                    if again == "1":
+                        break
                 else:
                     print('This field is taken. Choose other field')
             else:
                 print('Wrong field.' + 'Choose field from: ' + str(list(board1.keys())))
 
+
+
+
+#############################################################
--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 from functions import who_starts, print_board, choosing_field
-
 from functions import board1
 
-starts='X'
-
-win = False
 
 
+while True:
+    print_board(board1)
+
+    starts = who_starts()
+
+    print(starts)
+    print('-----')
+    print('1' + '|' + '2' + '|' + '3')
+    print('-----')
+    print('4' + '|' + '5' + '|' + '6')
+    print('-----')
+    print('7' + '|' + '8' + '|' + '9')
+    print('-----')
+    print('choose field please!: ' + str(list(board1.keys())))
+    choosing_field(starts)
 
 
 
-print_board(board1)
-
-who_starts()
-
-print(starts)
-print('-----')
-print('1' + '|' + '2' + '|' + '3')
-print('-----')
-print('4' + '|' + '5' + '|' + '6')
-print('-----')
-print('7' + '|' + '8' + '|' + '9')
-print('-----')
-print('choose field please!: ' + str(list(board1.keys())))
-
-choosing_field(starts)
